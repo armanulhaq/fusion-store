652c8e6a265a2f7552c07d7911d26ef29546b3a0
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-12-13 15:33:14 +0530

    fixed recommended filter

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -5,19 +5,19 @@ const Recommended = ({ handleClick }) => {
         <div>
             <h2 className="text-xl font-bold pl-5 pt-5 pb-3">Recommended</h2>
             <div className="flex gap-2 md:gap-4 lg:gap-5 px-5">
-                <Button variant="outline" onClick={handleClick}>
+                <Button variant="outline" onClick={handleClick} value="">
                     All
                 </Button>
-                <Button variant="outline" onClick={handleClick}>
-                    Nike
-                </Button>
-                <Button variant="outline" onClick={handleClick}>
+                <Button variant="outline" onClick={handleClick} value="Adidas">
                     Adidas
                 </Button>
-                <Button variant="outline" onClick={handleClick}>
+                <Button variant="outline" onClick={handleClick} value="Nike">
+                    Nike
+                </Button>
+                <Button variant="outline" onClick={handleClick} value="Puma">
                     Puma
                 </Button>
-                <Button variant="outline" onClick={handleClick}>
+                <Button variant="outline" onClick={handleClick} value="Vans">
                     Vans
                 </Button>
             </div>
