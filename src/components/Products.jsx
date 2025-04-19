f92ac71b6e58256dfa44daa35210c1d0999f33f3
Arman Ul Haq <armanulhaq10@gmail.com>
2025-04-19 13:50:21 +0530

    add no products found error

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,19 @@
 import ProductCard from "./ProductCard";
 
 const Products = ({ result }) => {
+    if (result.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center h-[50vh]">
+                <h2 className="text-2xl font-bold mb-4">No Products Found</h2>
+                <p className="text-gray-500">
+                    Try adjusting your filters or search query
+                </p>
+            </div>
+        );
+    }
+
     return (
-        <div className="grid grid-cols-1  px-4 lg:px-10 md:px-0 md:grid-cols-3 lg:grid-cols-4  my-4 md:my-4 lg:my-5 gap-5">
+        <div className="grid grid-cols-1 px-4 lg:px-10 md:px-0 md:grid-cols-3 lg:grid-cols-4 my-4 md:my-4 lg:my-5 gap-5">
             {result.map((product, index) => (
                 <ProductCard
                     key={index}
